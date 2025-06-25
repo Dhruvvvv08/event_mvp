572c7ba6c1793557cdfc4be878339c3082ddc0de
Dhruvvvv08 <madaandhruv51@gmail.com>
2025-06-26 00:48:54 +0530

    initial commit

---
+++ lib/features/auth/presentation/splashscreen.dart
@@ -0,0 +1,21 @@
+import 'package:event_mvp/core/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.k0a0a0a,
+      body: Column(
+        children: [Center(child: Image.asset("images/event_splash.jpeg"))],
+      ),
+    );
+  }
+}
